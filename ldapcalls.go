8074876350951c6075c267de9c6bb927981f7ce0
Bo Maryniuk <bo@suse.de>
2019-10-17 14:30:42 +0200

    Add init implementation of ldap caller

---
+++ ldapcalls.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-ldap/ldap"
+	"log"
+)
+
+type LDAPCaller struct {
+	user     string
+	password string
+	host     string
+	proto    string
+	port     int64
+	conn     *ldap.Conn
+}
+
+// Constructor of the LDAP caller with default options
+func NewLDAPCaller() *LDAPCaller {
+	lc := new(LDAPCaller)
+	lc.proto = "tcp"
+	lc.port = 389
+
+	return lc
+}
+
+func (lc *LDAPCaller) SetUser(user string) *LDAPCaller {
+	lc.user = user
+	return lc
+}
+
+func (lc *LDAPCaller) SetPassword(password string) *LDAPCaller {
+	lc.password = password
+	return lc
+}
+
+func (lc *LDAPCaller) SetPort(port int64) *LDAPCaller {
+	lc.port = port
+	return lc
+}
+
+func (lc *LDAPCaller) SetProto(proto string) *LDAPCaller {
+	lc.proto = proto
+	return lc
+}
+
+func (lc *LDAPCaller) SetHost(host string) *LDAPCaller {
+	lc.host = host
+	return lc
+}
+
+func (lc *LDAPCaller) Connect() {
+	var err error
+	lc.conn, err = ldap.Dial(lc.proto, fmt.Sprintf("%s:%d", lc.host, lc.port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Bound successfully.")
+}
+
+func (lc *LDAPCaller) Disconnect() {
+	if lc.conn != nil {
+		lc.conn.Close()
+		lc.conn = nil
+		fmt.Println("Closed")
+	}
+}
+
+func (lc *LDAPCaller) Search(request *ldap.SearchRequest) *ldap.SearchResult {
+	res, err := lc.conn.Search(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
+}
