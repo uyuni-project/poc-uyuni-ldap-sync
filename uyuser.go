afb8375172bd1fed665aeece9348e4716026bf39
Bo Maryniuk <bo@suse.de>
2019-10-21 16:20:47 +0200

    Add method for adding roles to the user

--- uyuser.go
+++ uyuser.go
@@ -1,15 +1,34 @@
 package main
 
+import (
+	"strings"
+)
+
 type UyuniUser struct {
 	Uid        string
 	Name       string
 	Secondname string
 	Email      string
 	Err        error
+	roles      []string
 }
 
 func NewUyuniUser() *UyuniUser {
-	return new(UyuniUser)
+	uu := new(UyuniUser)
+	uu.roles = make([]string, 0)
+
+	return uu
+}
+
+func (u *UyuniUser) AddRole(role string) {
+	role = strings.ToLower(role)
+	for _, userRole := range u.roles {
+		if userRole == role {
+			return
+		}
+	}
+
+	u.roles = append(u.roles, role)
 }
 
 // IsValid validates if the user data is compliant to the synchronised
