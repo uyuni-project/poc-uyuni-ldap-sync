98a97e4a1a2008af1d4115628bc73c8d7317f1a8
Bo Maryniuk <bo@suse.de>
2019-10-29 16:15:31 +0100

    Add utils src

---
+++ utils.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/thoas/go-funk"
+)
+
+// CompareRoles of two users
+func CompareRoles(a *UyuniUser, b *UyuniUser) bool {
+	ra := a.GetRoles()
+	rb := b.GetRoles()
+
+	if len(ra) != len(rb) {
+		return false
+	}
+
+	for _, r := range ra {
+		if !funk.Contains(rb, r) {
+			return false
+		}
+	}
+
+	return true
+}
